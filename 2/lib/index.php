99c45044e62cce6771e87d3114636a02e7b9324f
Giuseppe <laxeer0@gmail.com>
2021-01-11 16:34:28 +0100

    Second Commit and Finished

---
+++ 2/lib/index.php
@@ -0,0 +1,5 @@
+<?php
+    require_once('db.php');
+    $output_DB = json_encode($db);
+    echo $output_DB;
+?>
---
+++ 2/main.js
@@ -0,0 +1,29 @@
+var root = new Vue({
+    el: '#root',
+    data: {
+        apiUrl: "./lib/index.php",
+        albums: [],
+        currentGenre: 'all',
+
+    },
+    created() {
+        axios.get(this.apiUrl)
+            .then((response) => {
+                this.albums = response.data.response
+            });
+
+    },
+    computed: {
+        genreList() {
+            if (this.currentGenre == 'all') {
+                return this.albums
+            } else {
+                return this.albums.filter(album => {
+                    return album.genre.toLowerCase().includes(this.currentGenre.toLowerCase())
+                })
+            }
+        },
+
+    }
+
+});
